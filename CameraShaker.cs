95dd8783e50db1e20d695bcf404685110d53be3f
WillB <william.bele@edhec.com>
2021-07-08 17:03:30 +0200

    Add files via upload

---
+++ CameraShaker.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    public Vector3 mReferencePosition;
+
+    public float mShakeStrength;
+    public float mShakeDuration = 1f;
+    
+
+    private System.Random _mRand;
+
+    void Start()
+    {
+        _mRand = new System.Random();
+
+    }
+
+
+
+    public void Shake() {
+        mReferencePosition = transform.position;
+        StartCoroutine(Coroutine_Shake());
+
+    }
+
+
+    IEnumerator Coroutine_Shake() {
+        float elapsed = 0f;
+
+        Vector3 displacement = new Vector3();
+        do {
+            displacement.x = ((float)_mRand.NextDouble()) * 2f - 1f;
+            displacement.y = ((float)_mRand.NextDouble()) * 2f - 1f;
+            
+            //transform.position = mReferencePosition + displacement * mShakeStrength;
+            transform.position += displacement * mShakeStrength;
+
+
+            yield return null;
+
+            elapsed += Time.deltaTime;
+
+        } while (elapsed < mShakeDuration);
+
+        transform.position = mReferencePosition;
+
+        yield return null;
+    }
+}
